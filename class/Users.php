0f2a226002640dd404bc058692bb720b757eebed
YonaSmilevitch <EstherJudith92>
2018-04-26 08:36:22 +0200

    Merge branch 'master' of APPOE CORE
    # optimize functions

--- class/Users.php
+++ class/Users.php
@@ -167,7 +167,7 @@ class Users
   					`prenom` VARCHAR(100) NOT NULL,
   					`options` TEXT NULL DEFAULT NULL,
   					`statut` TINYINT(1) NOT NULL DEFAULT 1,
-                    `created_at` DATE NOT NULL DEFAULT DATE(NOW()),
+                    `created_at` DATE NULL,
                 	`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=170001;';
         $stmt = $this->dbh->prepare($sql);
