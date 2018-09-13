03778a780cfdd3cb0e6d072a893ee6efdba29e75
YonaSmilevitch <EstherJudith92>
2018-09-13 14:19:55 +0200

    Merge branch 'master' of APPOE
    # Optimizing

--- class/Users.php
+++ class/Users.php
@@ -3,7 +3,7 @@
 namespace App;
 class Users
 {
-    private $id;
+    private $id = null;
     private $email;
     private $password;
     private $role;
@@ -169,7 +169,7 @@ class Users
   					`statut` TINYINT(1) NOT NULL DEFAULT 1,
                     `created_at` DATE NULL,
                 	`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-				) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=157092;';
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=173812;';
         $stmt = $this->dbh->prepare($sql);
         $stmt->execute();
         $error = $stmt->errorInfo();
