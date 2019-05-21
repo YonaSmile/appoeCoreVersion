ce77a2c7bd18edde3554ae2ba13cab0e4dcfd546
YonaSmilevitch <EstherJudith92>
2019-05-21 12:18:38 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- page/getMediaDetails.php
+++ page/getMediaDetails.php
@@ -9,57 +9,70 @@ if (checkAjaxRequest() && !empty($_GET['fileId']) && is_numeric($_GET['fileId'])
     $Media = new \App\Media();
     $Media->setId($_GET['fileId']);
 
-    if ($Media->show()):
-        if (isImage(FILE_DIR_PATH . $Media->getName())):
-            $imgSize = getimagesize(FILE_DIR_PATH . $Media->getName());
-            ?>
-            <strong>Largeur:</strong> <?= $imgSize[0]; ?>px
-            <strong>|</strong> <strong>Hauteur:</strong> <?= $imgSize[1]; ?>px
-            <img src="<?= getThumb($Media->getName(), 370); ?>"
-                 alt="<?= $Media->getTitle(); ?>"
-                 data-originsrc="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"
-                 data-filename="<?= $Media->getName(); ?>"
-                 class="img-fluid seeOnOverlay">
-        <?php elseif (isAudio(FILE_DIR_PATH . $Media->getName())): ?>
-            <audio controls src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"></audio>
-        <?php elseif (isVideo(FILE_DIR_PATH . $Media->getName())): ?>
-            <video controls>
-                <source src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"
-                        type="<?= mime_content_type(FILE_DIR_PATH . $Media->getName()); ?>">
-            </video>
-        <?php else: ?>
-            <a href="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>" target="_blank">
-                <img src="<?= getImgAccordingExtension(getFileExtension($Media->getName())); ?>"
-                     data-filename="<?= $Media->getName(); ?>">
-            </a>
-        <?php endif; ?>
-        <h5 class="my-2" id="mediaTitle"><?= $Media->getTitle(); ?></h5>
+    if ($Media->show()): ?>
+        <div>
 
-        <small title="<?= trans('Copier le lien du média'); ?>">
-            <span class="copyLinkOnClick" data-src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>">
-                <?= WEB_DIR_INCLUDE . $Media->getName(); ?>
-            </span>
-            <a href="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>" target="_blank"
-               data-file-mime="<?= mime_content_type(FILE_DIR_PATH . $Media->getName()); ?>">
-                <i class="fas fa-external-link-alt mx-2"></i>
-            </a>
-        </small>
+            <?php if (isImage(FILE_DIR_PATH . $Media->getName())):
+                $imgSize = getimagesize(FILE_DIR_PATH . $Media->getName()); ?>
+                <div class="mb-2">
+                    <strong>Largeur:</strong> <?= $imgSize[0]; ?>px
+                    <strong>|</strong> <strong>Hauteur:</strong> <?= $imgSize[1]; ?>px
+                </div>
+                <img src="<?= getThumb($Media->getName(), 370); ?>"
+                     alt="<?= $Media->getTitle(); ?>"
+                     data-originsrc="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"
+                     data-filename="<?= $Media->getName(); ?>"
+                     class="img-fluid seeOnOverlay">
 
-        <form method="post" id="mediaDetailsForm" class="my-2">
-            <input type="hidden" name="id" value="<?= $Media->getId(); ?>">
-            <?= \App\Form::text('Titre', 'title', 'text', $Media->getTitle(), false, 255, '', '', 'form-control-sm imageTitle upImgForm', 'Titre'); ?>
-            <?= \App\Form::textarea('Description', 'description', $Media->getDescription(), 1, false, '', 'form-control-sm imageDescription upImgForm', 'Description'); ?>
-            <?= \App\Form::text('Lien', 'link', 'url', $Media->getLink(), false, 255, '', '', 'form-control-sm imagelink upImgForm', 'Lien'); ?>
-            <?= \App\Form::text('Position', 'position', 'text', $Media->getPosition(), false, 5, '', '', 'form-control-sm imagePosition upImgForm', 'Position'); ?>
-            <?= \App\Form::select('Bibliothèques', 'typeId', $listCatgories, $Media->getTypeId(), '', '', '', '', 'custom-select-sm imageTypeId upImgForm', false); ?>
-        </form>
+            <?php elseif (isAudio(FILE_DIR_PATH . $Media->getName())): ?>
+                <audio controls src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"></audio>
 
-        <small id="infosMedia" class="float-right"></small>
+            <?php elseif (isVideo(FILE_DIR_PATH . $Media->getName())): ?>
+                <video controls>
+                    <source src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>"
+                            type="<?= mime_content_type(FILE_DIR_PATH . $Media->getName()); ?>">
+                </video>
 
-        <button type="button" class="deleteImage btn btn-danger btn-sm"
-                data-imageid="<?= $Media->getId(); ?>" data-thumbwidth="370">
-            <i class="fas fa-times"></i> Supprimer
-        </button>
+            <?php else: ?>
+                <a href="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>" target="_blank">
+                    <img src="<?= getImgAccordingExtension(getFileExtension($Media->getName())); ?>"
+                         data-filename="<?= $Media->getName(); ?>">
+                </a>
+            <?php endif; ?>
+
+            <h5 class="my-2" id="mediaTitle"><?= $Media->getTitle(); ?></h5>
+
+            <small title="<?= trans('Copier le lien du média'); ?>">
+                <span class="copyLinkOnClick" data-src="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>">
+                    <?= WEB_DIR_INCLUDE . $Media->getName(); ?>
+                </span>
+                <a href="<?= WEB_DIR_INCLUDE . $Media->getName(); ?>" target="_blank"
+                   data-file-mime="<?= mime_content_type(FILE_DIR_PATH . $Media->getName()); ?>">
+                    <i class="fas fa-external-link-alt mx-2"></i>
+                </a>
+            </small>
+
+            <form method="post" id="mediaDetailsForm" class="my-2">
+                <input type="hidden" name="id" value="<?= $Media->getId(); ?>">
+                <?= \App\Form::text('Titre', 'title', 'text', $Media->getTitle(), false, 255, '', '', 'form-control-sm imageTitle upImgForm', 'Titre'); ?>
+                <?= \App\Form::textarea('Description', 'description', $Media->getDescription(), 1, false, '', 'form-control-sm imageDescription upImgForm', 'Description'); ?>
+                <?= \App\Form::text('Lien', 'link', 'url', $Media->getLink(), false, 255, '', '', 'form-control-sm imagelink upImgForm', 'Lien'); ?>
+                <?= \App\Form::text('Position', 'position', 'text', $Media->getPosition(), false, 5, '', '', 'form-control-sm imagePosition upImgForm', 'Position'); ?>
+                <?= \App\Form::select('Bibliothèques', 'typeId', $listCatgories, $Media->getTypeId(), '', '', '', '', 'custom-select-sm imageTypeId upImgForm', false); ?>
+            </form>
+
+            <small id="infosMedia" class="float-right text-success"></small>
+
+            <hr class="mx-5 my-3">
+            <button type="button" class="closeMediaDetails btn btn-outline-secondary btn-sm float-left">
+                Fermer <i class="fas fa-chevron-right"></i>
+            </button>
+
+            <button type="button" class="deleteImage btn btn-outline-danger btn-sm float-right"
+                    data-imageid="<?= $Media->getId(); ?>" data-thumbwidth="370">
+                <i class="fas fa-times"></i> Supprimer
+            </button>
+        </div>
     <?php endif; ?>
 <?php else: ?>
     <p><?= trans('Ce fichier n\'existe pas'); ?> !</p>
--- page/updateMedia.php
+++ page/updateMedia.php
@@ -10,91 +10,87 @@ $allLibrary = extractFromObjToSimpleArr($Category->showAll(), 'id', 'name');
 $Category->setType('MEDIA');
 $listCatgories = extractFromObjToArrForList($Category->showByType(), 'id');
 echo getTitle($Page->getName(), $Page->getSlug()); ?>
-    <div id="mediaContainer" class="row">
-        <div class="col-12 col-md-8 col-lg-9">
-            <nav>
-                <!--<div class="float-right">
-                    <button type="button" role="button" class="btn btn-sm listView">
-                        <i class="fas fa-th-list"></i>
-                    </button>
-                </div>-->
-                <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                    <a class="nav-item nav-link active" id="nav-allLibraries-tab" data-toggle="tab"
-                       href="#nav-allLibraries"
-                       role="tab" aria-controls="nav-allLibraries"
-                       aria-selected="true"><?= trans('Les bibliothèques'); ?></a>
-                    <a class="nav-item nav-link" id="nav-newFiles-tab" data-toggle="tab" href="#nav-newFiles" role="tab"
-                       aria-controls="nav-newFiles" aria-selected="false"><?= trans('Téléchargement des médias'); ?></a>
-                </div>
-            </nav>
-            <div class="tab-content border border-top-0 bg-white py-3" id="nav-mediaTabContent">
-                <div class="tab-pane fade show active" id="nav-allLibraries" role="tabpanel"
-                     aria-labelledby="nav-home-tab">
-                    <?php if ($allLibrary): ?>
-                        <div class="container-fluid">
-                            <div id="shortAccessBtns" class="mb-4 float-right">
-                                <button type="button" class="btn btn-sm btn-secondary"
-                                        data-libraryid="all"><?= trans('Tous'); ?></button>
-                            </div>
-                            <?php foreach ($allLibrary as $id => $name):
-                                $Media->setTypeId($id);
-                                $allFiles = $Media->showFiles();
-                                if ($allFiles): ?>
-                                    <div class="mediaContainer" data-libraryid="media-<?= $id; ?>">
-                                        <h5 class="libraryName p-3" id="media-<?= $id; ?>"><?= $name; ?></h5>
-                                        <hr class="my-3 mx-5">
-                                        <div class="card-columns">
-                                            <?php foreach ($allFiles as $file): ?>
-                                                <div class="card fileContent bg-none border-0"
-                                                     data-file-id="<?= $file->id; ?>">
-                                                    <a href="#" class="getMediaDetails"
-                                                       data-file-id="<?= $file->id; ?>">
-                                                        <?php if (isImage(FILE_DIR_PATH . $file->name)): ?>
-                                                            <img src="<?= getThumb($file->name, 370); ?>"
-                                                                 class="img-fluid draggable">
-                                                        <?php else: ?>
-                                                            <img src="<?= getImgAccordingExtension(getFileExtension($file->name)); ?>"
-                                                                 class="draggable">
-                                                        <?php endif; ?>
-                                                    </a>
-                                                </div>
-                                            <?php endforeach; ?>
-                                        </div>
-                                        <div class="my-3"></div>
-                                    </div>
-                                <?php endif;
-                            endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="tab-pane fade" id="nav-newFiles" role="tabpanel" aria-labelledby="nav-profile-tab">
+    <div id="mediaContainer">
+        <nav>
+            <!--<div class="float-right">
+                <button type="button" role="button" class="btn btn-sm listView">
+                    <i class="fas fa-th-list"></i>
+                </button>
+            </div>-->
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="nav-item nav-link active" id="nav-allLibraries-tab" data-toggle="tab"
+                   href="#nav-allLibraries"
+                   role="tab" aria-controls="nav-allLibraries"
+                   aria-selected="true"><?= trans('Les bibliothèques'); ?></a>
+                <a class="nav-item nav-link" id="nav-newFiles-tab" data-toggle="tab" href="#nav-newFiles" role="tab"
+                   aria-controls="nav-newFiles" aria-selected="false"><?= trans('Téléchargement des médias'); ?></a>
+            </div>
+        </nav>
+        <div class="tab-content border border-top-0 bg-white py-3" id="nav-mediaTabContent">
+            <div class="tab-pane fade show active" id="nav-allLibraries" role="tabpanel"
+                 aria-labelledby="nav-home-tab">
+                <?php if ($allLibrary): ?>
                     <div class="container-fluid">
-                        <form class="row" id="galleryForm" action="" method="post" enctype="multipart/form-data">
-                            <?= getTokenField(); ?>
-                            <div class="col-12 col-lg-6 my-2">
-                                <?= \App\Form::file('Importer des médias', 'inputFile[]', false, 'multiple'); ?>
-                            </div>
-                            <div class="col-12 col-lg-3 my-2">
+                        <div id="shortAccessBtns" class="mb-4 float-right">
+                            <button type="button" class="btn btn-sm btn-secondary"
+                                    data-libraryid="all"><?= trans('Tous'); ?></button>
+                        </div>
+                        <?php foreach ($allLibrary as $id => $name):
+                            $Media->setTypeId($id);
+                            $allFiles = $Media->showFiles();
+                            if ($allFiles): ?>
+                                <div class="mediaContainer" data-libraryid="media-<?= $id; ?>">
+                                    <h5 class="libraryName p-3" id="media-<?= $id; ?>"><?= $name; ?></h5>
+                                    <hr class="my-3 mx-5">
+                                    <div class="card-columns">
+                                        <?php foreach ($allFiles as $file): ?>
+                                            <div class="card fileContent bg-none border-0"
+                                                 data-file-id="<?= $file->id; ?>">
+                                                <a href="#" class="getMediaDetails"
+                                                   data-file-id="<?= $file->id; ?>">
+                                                    <?php if (isImage(FILE_DIR_PATH . $file->name)): ?>
+                                                        <img src="<?= getThumb($file->name, 370); ?>"
+                                                             class="img-fluid draggable">
+                                                    <?php else: ?>
+                                                        <img src="<?= getImgAccordingExtension(getFileExtension($file->name)); ?>"
+                                                             class="draggable">
+                                                    <?php endif; ?>
+                                                </a>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                    <div class="my-3"></div>
+                                </div>
+                            <?php endif;
+                        endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <div class="tab-pane fade" id="nav-newFiles" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div class="container-fluid">
+                    <form class="row" id="galleryForm" action="" method="post" enctype="multipart/form-data">
+                        <?= getTokenField(); ?>
+                        <div class="col-12 col-lg-6 my-2">
+                            <?= \App\Form::file('Importer des médias', 'inputFile[]', false, 'multiple'); ?>
+                        </div>
+                        <div class="col-12 col-lg-3 my-2">
                                 <textarea name="textareaSelectedFile" id="textareaSelectedFile"
                                           class="d-none"></textarea>
-                                <?= \App\Form::text('Choisissez des médias', 'inputSelectFiles', 'text', '0 fichiers', false, 300, 'readonly data-toggle="modal" data-target="#allMediasModal"'); ?>
-                            </div>
-                            <div class="col-12 col-lg-3 my-2">
-                                <?= \App\Form::select('Bibliothèques', 'library', $listCatgories, '', true); ?>
-                            </div>
-                            <div class="col-12">
-                                <?= \App\Form::target('ADDIMAGES'); ?>
-                                <?= \App\Form::submit('Enregistrer', 'addImageSubmit'); ?>
-                            </div>
-                        </form>
-                    </div>
+                            <?= \App\Form::text('Choisissez des médias', 'inputSelectFiles', 'text', '0 fichiers', false, 300, 'readonly data-toggle="modal" data-target="#allMediasModal"'); ?>
+                        </div>
+                        <div class="col-12 col-lg-3 my-2">
+                            <?= \App\Form::select('Bibliothèques', 'library', $listCatgories, '', true); ?>
+                        </div>
+                        <div class="col-12">
+                            <?= \App\Form::target('ADDIMAGES'); ?>
+                            <?= \App\Form::submit('Enregistrer', 'addImageSubmit'); ?>
+                        </div>
+                    </form>
                 </div>
-                <div class="my-4"></div>
             </div>
+            <div class="my-4"></div>
         </div>
-        <div class="col-12 col-md-4 col-lg-3 position-relative">
-            <div class="position-fixed p-2" id="mediaDetails" data-file-id="">Aucun fichier sélectionné</div>
-        </div>
+        <div id="mediaDetails" data-file-id="">Aucun fichier sélectionné</div>
         <div class="modal fade" id="allMediasModal" tabindex="-1" role="dialog" aria-labelledby="allMediasModalLabel"
              aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
