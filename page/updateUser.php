7b50a22ccaf735e3688508e344918001ee6b5f7c
YonaSmilevitch <EstherJudith92>
2020-01-15 18:45:27 +0100

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- page/updateUser.php
+++ page/updateUser.php
@@ -15,21 +15,24 @@ if (!empty($_GET['id'])):
             </div>
         <?php endif; ?>
         <div class="row">
-            <div class="col-12 col-lg-6 my-2">
+            <div class="col-lg-12 col-xl-8 my-2">
                 <form action="" method="post" id="updateUserForm">
                     <?= getTokenField(); ?>
                     <input type="hidden" name="id" value="<?= $UpdateUser->getId() ?>">
                     <div class="row">
-                        <div class="col-12 my-2">
+                        <div class="col-md-12 col-lg-6 my-2">
                             <?php
                             $help = '<small id="loginHelp" class="form-text text-muted">' . trans('En changeant votre login vous serez déconnecté du logiciel') . '</small>';
                             echo \App\Form::text('Login', 'login', 'text', $UpdateUser->getLogin(), true, 70, 'aria-describedby="loginHelp"', $UpdateUser->getId() == getUserIdSession() ? $help : '');
                             ?>
                         </div>
-                        <div class="col-12 my-2">
+                        <div class="col-md-12 col-lg-6 my-2">
+                            <?= \App\Form::text('Email', 'email', 'email', $UpdateUser->getEmail(), false); ?>
+                        </div>
+                        <div class="col-md-12 col-lg-6 my-2">
                             <?= \App\Form::text('Nom', 'nom', 'text', $UpdateUser->getNom(), true, 40); ?>
                         </div>
-                        <div class="col-12 my-2">
+                        <div class="col-md-12 col-lg-6 my-2">
                             <?= \App\Form::text('Prénom', 'prenom', 'text', $UpdateUser->getPrenom(), true, 40); ?>
                         </div>
                         <?php if ($UpdateUser->getId() != getUserIdSession() && $UpdateUser->getRole() < (getTechnicienRoleId() + 1)): ?>
@@ -37,38 +40,34 @@ if (!empty($_GET['id'])):
                                 <?= \App\Form::select('Rôle', 'role', array_map('trans', getRoles()), $UpdateUser->getRole(), true, '', getUserRoleId(), '>'); ?>
                             </div>
                         <?php endif; ?>
-                        <div class="col-12 my-2">
-                            <?= \App\Form::text('Email', 'email', 'email', $UpdateUser->getEmail(), false); ?>
-                        </div>
-                    </div>
-                    <div class="my-2"></div>
-                    <div class="row">
-                        <div class="col-12">
+                        <div class="col-12 my-3">
                             <?= \App\Form::target('UPDATEUSER'); ?>
-                            <?= \App\Form::submit('Enregistrer', 'UPDATEUSERSUBMIT'); ?>
+                            <?= \App\Form::submit('Enregistrer', 'UPDATEUSERSUBMIT', 'btn-outline-dark'); ?>
                         </div>
                     </div>
                 </form>
             </div>
             <hr class="hrStyle d-lg-none d-md-block my-4">
-            <div class="col-12 col-lg-6 my-2">
+            <div class="col-lg-12 col-xl-4 my-2">
                 <form action="" method="post" id="updatePasswordUserForm" autocomplete="off">
                     <input type="hidden" name="_token" value="<?= getToken() ?>">
                     <input type="hidden" name="id" value="<?= $UpdateUser->getId() ?>">
                     <div class="row">
-                        <div class="col-12 my-2">
-                            <?= \App\Form::text('Nouveau Mot de passe', 'password', 'password', 'password', true, 150, 'autocomplete="off"'); ?>
-                        </div>
-
-                        <div class="col-12 my-2">
-                            <?= \App\Form::text('Confirmation du Mot de passe', 'password2', 'password', '', true, 150, 'autocomplete="off"'); ?>
-                        </div>
-                    </div>
-                    <div class="my-2"></div>
-                    <div class="row">
                         <div class="col-12">
-                            <?= \App\Form::target('UPDATEPASSWORD'); ?>
-                            <?= \App\Form::submit('Enregistrer', 'UPDATEPASSWORDSUBMIT'); ?>
+                            <div class="card bgColorPrimary border">
+                                <div class="card-body row">
+                                    <div class="col-12 my-2">
+                                        <?= \App\Form::text('Nouveau Mot de passe', 'password', 'password', 'password', true, 150, 'autocomplete="off"'); ?>
+                                    </div>
+                                    <div class="col-12 my-2">
+                                        <?= \App\Form::text('Confirmation du Mot de passe', 'password2', 'password', '', true, 150, 'autocomplete="off"'); ?>
+                                    </div>
+                                    <div class="col-12 my-2">
+                                        <?= \App\Form::target('UPDATEPASSWORD'); ?>
+                                        <?= \App\Form::submit('Enregistrer', 'UPDATEPASSWORDSUBMIT', 'btn-outline-light'); ?>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </form>
