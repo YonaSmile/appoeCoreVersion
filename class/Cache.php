9ba5fa201897f52aee2090761ebe80e5c4f6dabb
YonaSmilevitch <EstherJudith92>
2020-08-07 13:19:48 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- class/Cache.php
+++ class/Cache.php
@@ -4,95 +4,95 @@ namespace App;
 
 class Cache {
 
-	public $dirname = CACHE_PATH . LANG . DIRECTORY_SEPARATOR;
-	public $filename;
-	public $file;
-	public $duration = CACHE_DURATION; // In minutes
+    public $dirname = CACHE_PATH . LANG . DIRECTORY_SEPARATOR;
+    public $filename;
+    public $file;
+    public $duration = CACHE_DURATION; // In minutes
 
-	private $buffer = false;
+    private $buffer = false;
 
-	public function __construct( $filename ) {
+    public function __construct( $filename ) {
 
-		$this->filename = $filename;
+        $this->filename = $filename;
 
-		if ( ! is_dir( $this->dirname ) ) {
-			mkdir( $this->dirname, 0755, true );
-		}
+        if ( ! is_dir( $this->dirname ) ) {
+            mkdir( $this->dirname, 0755, true );
+        }
 
-		$this->file = $this->dirname . $this->filename;
-	}
+        $this->file = $this->dirname . $this->filename;
+    }
 
-	/**
-	 * Read from cache file
-	 * @return bool|false|string
-	 */
-	public function read() {
+    /**
+     * Read from cache file
+     * @return bool|false|string
+     */
+    public function read() {
 
-		if ( file_exists( $this->file ) ) {
-			$lifetime = ( time() - filemtime( $this->file ) ) / 60;
-			if ( $lifetime > $this->duration ) {
-				return false;
-			}
+        if ( file_exists( $this->file ) ) {
+            $lifetime = ( time() - filemtime( $this->file ) ) / 60;
+            if ( $lifetime > $this->duration ) {
+                return false;
+            }
 
-			return file_get_contents( $this->file );
-		}
+            return file_get_contents( $this->file );
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * write in cache file
-	 *
-	 * @param $content
-	 */
-	public function write( $content ) {
-		file_put_contents( $this->file, $content );
-	}
+    /**
+     * write in cache file
+     *
+     * @param $content
+     */
+    public function write( $content ) {
+        file_put_contents( $this->file, $content );
+    }
 
-	/**
-	 * Starting write in cache file if APPOE isn't in maintenance mode
-	 * @return bool
-	 */
-	public function start() {
+    /**
+     * Starting write in cache file if APPOE isn't in maintenance mode
+     * @return bool
+     */
+    public function start() {
 
-		$AppConfig = new AppConfig();
-		if ( 'false' === $AppConfig->get( 'options', 'cacheProcess' ) || 'true' === $AppConfig->get( 'options', 'maintenance' ) ) {
-			return false;
-		}
+        $AppConfig = new AppConfig();
+        if ( 'false' == $AppConfig->get( 'options', 'cacheProcess' ) || 'true' == $AppConfig->get( 'options', 'maintenance' ) ) {
+            return false;
+        }
 
-		if ( $content = $this->read() ) {
-			echo $content;
+        if ( $content = $this->read() ) {
+            echo $content;
 
-			return true;
-		}
-		$this->buffer = true;
-		ob_start();
+            return true;
+        }
+        $this->buffer = true;
+        ob_start();
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * end writing in cache file
-	 * @return bool
-	 */
-	public function end() {
-		if ( ! $this->buffer ) {
-			return false;
-		}
-		$content = ob_get_clean();
-		echo $content;
-		$this->write( $content );
+    /**
+     * end writing in cache file
+     * @return bool
+     */
+    public function end() {
+        if ( ! $this->buffer ) {
+            return false;
+        }
+        $content = ob_get_clean();
+        echo $content;
+        $this->write( $content );
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * delete cache file
-	 */
-	public function delete() {
+    /**
+     * delete cache file
+     */
+    public function delete() {
 
-		if ( file_exists( $this->file ) ) {
-			unlink( $this->file );
-		}
-	}
+        if ( file_exists( $this->file ) ) {
+            unlink( $this->file );
+        }
+    }
 }
