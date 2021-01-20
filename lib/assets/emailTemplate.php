e08b845ceeb88d859f5442784230b36b62c4f051
YonaSmilevitch <EstherJudith92>
2021-01-20 12:18:24 +0100

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- lib/assets/emailTemplate.php
+++ lib/assets/emailTemplate.php
@@ -356,7 +356,7 @@
                 <div class="footer">
                     <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                         <tr>
-                            <td class="content-block powered-by"><?= getAppoeCredit(); ?></td>
+                            <td class="content-block powered-by"><?= WEB_TITLE; ?></td>
                         </tr>
                     </table>
                 </div>
