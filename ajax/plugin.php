3755d462e0b178c51d59327e47304792ad25f22f
YonaSmilevitch <EstherJudith92>
2021-04-01 13:03:28 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- ajax/plugin.php
+++ ajax/plugin.php
@@ -1,66 +1,65 @@
 <?php
-require_once( 'header.php' );
+require_once('header.php');
 
 use App\DB;
-use App\Form;
 use App\Menu;
 use App\Plugin\Cms\Cms;
 use App\Plugin\ItemGlue\Article;
 
-if ( checkAjaxRequest() ) {
+if (checkAjaxRequest()) {
 
-    if ( ! empty( $_POST['checkUserSession'] ) ) {
+    if (!empty($_POST['checkUserSession'])) {
         echo isUserSessionExist() ? 'true' : 'false';
         exit();
     }
 
-    if ( ! empty( $_REQUEST['setupPath'] ) ) {
-        activePlugin( $_REQUEST['setupPath'] );
+    if (!empty($_REQUEST['setupPath'])) {
+        activePlugin($_REQUEST['setupPath']);
         exit();
     }
 
-    if ( ! empty( $_REQUEST['deletePluginName'] ) ) {
+    if (!empty($_REQUEST['deletePluginName'])) {
 
         $pluginName = $_REQUEST['deletePluginName'];
 
         //Backup
-        DB::backup( date( 'Y-m-d' ), 'db-' . date( 'H_i_s' ) );
-        saveFiles( 'app/plugin/' . $pluginName );
+        DB::backup(date('Y-m-d'), 'db-' . date('H_i_s'));
+        saveFiles('app/plugin/' . $pluginName);
 
         //Delete tables
-        if ( file_exists( WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php' ) ) {
-            include_once( WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php' );
+        if (file_exists(WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php')) {
+            include_once(WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php');
 
-            if ( ! empty( PLUGIN_TABLES ) ) {
-                foreach ( PLUGIN_TABLES as $key => $table ) {
-                    DB::initialize()->deleteTable( $table );
+            if (!empty(PLUGIN_TABLES)) {
+                foreach (PLUGIN_TABLES as $key => $table) {
+                    DB::initialize()->deleteTable($table);
                 }
             }
         }
 
         //Delete Menu
         $Menu = new Menu();
-        $Menu->deletePluginMenu( $pluginName );
+        $Menu->deletePluginMenu($pluginName);
 
         //Delete Directories & Files
-        deleteAllFolderContent( WEB_PLUGIN_PATH . $pluginName );
+        deleteAllFolderContent(WEB_PLUGIN_PATH . $pluginName);
 
-        echo json_encode( true );
+        echo json_encode(true);
         exit();
     }
 
-    if ( ! empty( $_POST['checkTable'] ) ) {
+    if (!empty($_POST['checkTable'])) {
 
         $pluginName = $_POST['checkTable'];
         $existTable = 0;
 
-        if ( file_exists( WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php' ) ) {
-            include_once( WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php' );
+        if (file_exists(WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php')) {
+            include_once(WEB_PLUGIN_PATH . $pluginName . DIRECTORY_SEPARATOR . 'ini.php');
 
-            if ( ! empty( PLUGIN_TABLES ) ) {
-                foreach ( PLUGIN_TABLES as $key => $table ) {
-                    if ( DB::initialize()->checkTable( $table ) ) {
-                        $existTable ++;
+            if (!empty(PLUGIN_TABLES)) {
+                foreach (PLUGIN_TABLES as $key => $table) {
+                    if (DB::initialize()->checkTable($table)) {
+                        $existTable++;
                     }
                 }
             }
@@ -71,37 +70,38 @@ if ( checkAjaxRequest() ) {
     }
 
     //Github links
-    $gitHub            = 'https://github.com/YonaSmile/';
+    $gitHub = 'https://github.com/YonaSmile/';
     $gitHubUserContent = 'https://raw.githubusercontent.com/YonaSmile/';
+    //$gitHubAccessToken = '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';
 
     //Plugin version
-    if ( ! empty( $_POST['checkVersion'] ) ) {
-        echo getHttpRequest( $gitHubUserContent . 'appoePluginsVersions/master/' . $_POST['checkVersion'] . '/version.json' );
+    if (!empty($_POST['checkVersion'])) {
+        echo getHttpRequest($gitHubUserContent . 'appoePluginsVersions/master/' . $_POST['checkVersion'] . '/version.json');
         exit();
     }
 
     //Core version
-    if ( ! empty( $_POST['checkSystemVersion'] ) ) {
-        echo getHttpRequest( $gitHubUserContent . 'appoeCoreVersion/master/version.json' );
+    if (!empty($_POST['checkSystemVersion'])) {
+        echo getHttpRequest($gitHubUserContent . 'appoeCoreVersion/master/version.json');
         exit();
     }
 
-    if ( ! empty( $_POST['downloadPlugins'] ) ) {
+    if (!empty($_POST['downloadPlugins'])) {
 
-        if ( downloadZip( ROOT_PATH . 'plugins.zip', $gitHub . 'appoePluginsVersions/archive/master.zip' ) ) {
-            if ( unzipSkipFirstFolder( ROOT_PATH . 'plugins.zip', ROOT_PATH, 'appoePluginsVersions-master', WEB_PLUGIN_PATH ) ) {
+        if (downloadZip(ROOT_PATH . 'plugins.zip', $gitHub . 'appoePluginsVersions/archive/refs/heads/master.zip')) {
+            if (unzipSkipFirstFolder(ROOT_PATH . 'plugins.zip', ROOT_PATH, 'appoePluginsVersions-master', WEB_PLUGIN_PATH)) {
                 echo 'true';
             }
         }
         exit();
     }
 
-    if ( ! empty( $_POST['downloadSystemCore'] ) ) {
+    if (!empty($_POST['downloadSystemCore'])) {
 
-        if ( downloadZip( ROOT_PATH . 'appoeCore.zip', $gitHub . 'appoeCoreVersion/archive/master.zip' ) ) {
-            if ( unzipSkipFirstFolder( ROOT_PATH . 'appoeCore.zip', ROOT_PATH, 'appoeCoreVersion-master', WEB_APP_PATH ) ) {
-                if ( downloadZip( ROOT_PATH . 'rooter.zip', $gitHub . 'appoeRooterVersion/archive/master.zip' ) ) {
-                    if ( unzipSkipFirstFolder( ROOT_PATH . 'rooter.zip', ROOT_PATH, 'appoeRooterVersion-master', ROOT_PATH ) ) {
+        if (downloadZip(ROOT_PATH . 'appoeCore.zip', $gitHub . 'appoeCoreVersion/archive/refs/heads/master.zip')) {
+            if (unzipSkipFirstFolder(ROOT_PATH . 'appoeCore.zip', ROOT_PATH, 'appoeCoreVersion-master', WEB_APP_PATH)) {
+                if (downloadZip(ROOT_PATH . 'rooter.zip', $gitHub . 'appoeRooterVersion/archive/refs/heads/master.zip')) {
+                    if (unzipSkipFirstFolder(ROOT_PATH . 'rooter.zip', ROOT_PATH, 'appoeRooterVersion-master', ROOT_PATH)) {
 
                         updateDB();
                         echo 'true';
@@ -112,45 +112,45 @@ if ( checkAjaxRequest() ) {
         exit();
     }
 
-    if ( ! empty( $_POST['updateSitemap'] ) ) {
+    if (!empty($_POST['updateSitemap'])) {
 
         $data = array();
 
         //Get Pages Slug
-        $Cms   = new Cms();
+        $Cms = new Cms();
         $pages = $Cms->showAllPages();
 
-        if ( $pages ) {
-            foreach ( $pages as $page ) {
-                if ( file_exists( WEB_PATH . $page->filename . '.php' ) ) {
-                    $data[] = array( 'slug' => webUrl( $page->slug . '/' ), 'name' => $page->name );
+        if ($pages) {
+            foreach ($pages as $page) {
+                if (file_exists(WEB_PATH . $page->filename . '.php')) {
+                    $data[] = array('slug' => webUrl($page->slug . '/'), 'name' => $page->name);
                 }
             }
         }
 
         //Get Articles Slug
-        if ( defined( 'DEFAULT_ARTICLES_PAGE' ) && ! empty( DEFAULT_ARTICLES_PAGE ) ) {
+        if (defined('DEFAULT_ARTICLES_PAGE') && !empty(DEFAULT_ARTICLES_PAGE)) {
 
-            $Article  = new Article();
+            $Article = new Article();
             $articles = $Article->showAll();
 
-            if ( $articles ) {
-                foreach ( $articles as $article ) {
+            if ($articles) {
+                foreach ($articles as $article) {
                     $data[] = array(
-                        'slug' => webUrl( DEFAULT_ARTICLES_PAGE . '/' . $article->slug ),
+                        'slug' => webUrl(DEFAULT_ARTICLES_PAGE . '/' . $article->slug),
                         'name' => $article->name
                     );
                 }
             }
         }
 
-        if ( generateSitemap( $data ) ) {
+        if (generateSitemap($data)) {
             echo 'true';
         }
         exit();
     }
 
-    if ( ! empty( $_POST['optimizeDb'] ) ) {
+    if (!empty($_POST['optimizeDb'])) {
 
         /*$tables = DB::initialize()->getTables();
         $Menu = new Menu();
@@ -167,25 +167,27 @@ if ( checkAjaxRequest() ) {
                 }
             }
         }*/
-
-        DB::backup( date( 'Y-m-d' ), 'db-' . date( 'H_i' ) );
-        echo trans( 'Tables enregistrées' );// . '.' . (count($optimizeTables) > 0 ? '<br><strong>Tables supprimées:</strong> ' . implode(', ', $optimizeTables) : '');
+        $dbFolderName = date('Y-m-d');
+        $dbFileName = 'db-' . date('H_i');
+        DB::backup($dbFolderName, $dbFileName);
+        $urlFile = WEB_APP_URL . 'backup/' . $dbFolderName . '/' . $dbFileName . '.sql.gz';
+        echo trans('Tables enregistrées') . '. <a href="' . $urlFile . '"> ' . trans('Télécharger') . '</a>';// . '.' . (count($optimizeTables) > 0 ? '<br><strong>Tables supprimées:</strong> ' . implode(', ', $optimizeTables) : '');
         exit();
     }
 
-    if ( ! empty( $_POST['saveFile'] ) ) {
+    if (!empty($_POST['saveFile'])) {
 
-        $folder    = ! empty( $_POST['folder'] ) ? $_POST['folder'] : 'public';
-        $saveFiles = saveFiles( $folder );
-        if ( false !== $saveFiles ) {
-            echo $folder . ' / ' . getSizeName( $saveFiles['copySize'] ) . trans( ' Fichiers enregistrés' ), '. <a href="' . $saveFiles['downloadLink'] . '"> ' . trans( 'Télécharger' ) . ' (' . getSizeName( $saveFiles['zipSize'] ) . ')</a>';
+        $folder = !empty($_POST['folder']) ? $_POST['folder'] : 'public';
+        $saveFiles = saveFiles($folder);
+        if (false !== $saveFiles) {
+            echo $folder . ' / ' . getSizeName($saveFiles['copySize']) . trans(' Fichiers enregistrés'), '. <a href="' . $saveFiles['downloadLink'] . '"> ' . trans('Télécharger') . ' (' . getSizeName($saveFiles['zipSize']) . ')</a>';
         }
         exit();
     }
 
-    if ( ! empty( $_POST['getDefinedConst'] ) ) {
+    if (!empty($_POST['getDefinedConst'])) {
 
-        echo defined( $_POST['getDefinedConst'] ) ? constant( $_POST['getDefinedConst'] ) : '';
+        echo defined($_POST['getDefinedConst']) ? constant($_POST['getDefinedConst']) : '';
         exit();
     }
 }
