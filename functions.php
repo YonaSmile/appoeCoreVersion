d5d272cfa3913b410ac28c8c9a72b8e6a0f9af43
YonaSmilevitch <EstherJudith92>
2021-05-26 18:00:41 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- functions.php
+++ functions.php
@@ -3415,7 +3415,8 @@ function includePluginsJs($forApp = false, $min = false)
             }
         }
     }
-    echo '<script type="text/javascript" src="' . WEB_LIB_URL . 'js/functions.js"></script>';
+    echo '<script type="text/javascript" src="' . WEB_LIB_URL . 'js/functions.js"></script>'
+        . '<script type="text/javascript" src="' . WEB_LIB_URL . 'js/public.js"></script>';
 
     if ($forApp) {
         includePluginsJsForApp();
