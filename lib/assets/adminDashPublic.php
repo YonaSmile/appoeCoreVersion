ec1c5e6f1f1f57774de016391da7450b2631f483
YonaSmile <yonasmilevitch@gmail.com>
2021-06-27 14:56:29 +0200

    Add APPOE logo & favicon

--- lib/assets/adminDashPublic.php
+++ lib/assets/adminDashPublic.php
@@ -1,54 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/app/main.php');
 if (isUserAuthorized('updatePageContent')): ?>
-    <style>
-        #adminDashPublic {
-            position: fixed;
-            bottom: 20%;
-            left: 0;
-            transform: translateY(-20%);
-            box-sizing: border-box;
-            z-index: 999999;
-            text-align: left;
-        }
-
-        #adminDashPublic a,
-        #adminDashPublic button {
-            width: 35px;
-            transition: all 0.2s;
-            display: block;
-            color: #FFF;
-            background: rgba(0, 0, 0, 0.3);
-            font-size: 13px;
-            border: 0;
-            padding: 10px 0;
-            line-height: 16px;
-            text-align: center;
-        }
-
-        #adminDashPublic a img,
-        #adminDashPublic button img {
-            width: 17px;
-        }
-
-        #adminDashPublic hr {
-            width: 35px;
-            display: block;
-            color: #FFF;
-            background: #FFF;
-            border: 0;
-            padding: 0;
-            margin: 0;
-            height: 1px;
-        }
-
-        #adminDashPublic a:focus,
-        #adminDashPublic a:hover,
-        #adminDashPublic button:focus,
-        #adminDashPublic button:hover {
-            background: rgba(0, 0, 0, 0.8);
-        }
-    </style>
     <div id="adminDashPublic">
         <a href="<?= WEB_ADMIN_URL; ?>" title="Tableau de bord">
             <img src="<?= APP_IMG_URL; ?>dashboard.svg" alt="">
