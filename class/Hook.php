8baeb5e298ce9abad5f294a4eb4141d26b53da88
YonaSmilevitch <EstherJudith92>
2021-07-29 15:20:59 +0200

    Merge branch 'master' of APPOE
    # Optimizing system & plugins

--- class/Hook.php
+++ class/Hook.php
@@ -5,6 +5,7 @@ class Hook
 {
     private static $defaultPriority = 100;
     private static $HOOKS = array(
+        'cron' => array(),
         'core_admin_before_dashboard' => array(),
         'core_admin_after_dashboard' => array(),
         'core_front_before_html' => array()
