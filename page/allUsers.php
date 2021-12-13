27f17f618e8714deceda07906e1120eaad0d827f
YonaSmilevitch <EstherJudith92>
2021-12-13 17:51:53 +0100

    Merge branch 'master' of APPOE
    # Change directory adding rooter to core

--- page/allUsers.php
+++ page/allUsers.php
@@ -1,70 +1,86 @@
 <?php require('header.php');
 echo getTitle(getAppPageName(), getAppPageSlug());
 $defaultEmail = getOptionData('defaultEmail'); ?>
+    <div id="admin-tab-search">
+        <input type="search" class="form-control" id="admin-tab-search-input" placeholder="Rechercher...">
+    </div>
+    <div id="admin-tabs-menu" class="row d-none d-sm-flex">
+        <div class="d-none d-sm-block col-sm-4 col-md-3 col-lg-3 col-xl-3">Login</div>
+        <div class="d-none d-sm-block col-sm-4 col-md-3 col-lg-2 col-xl-2">Nom</div>
+        <div class="d-none d-lg-block col-lg-2 col-xl-2">Prénom</div>
+        <div class="d-none d-md-block col-md-3 col-lg-3 col-xl-3">Email</div>
+        <div class="d-none d-sm-block col-sm-4 col-md-3 col-lg-2 col-xl-2">Rôle</div>
+    </div>
+<?php if (defined('ALLUSERS')): ?>
     <div class="row">
-        <div class="col-12">
-            <div class="table-responsive">
-                <table id="clientTable"
-                       class="sortableTable table table-striped">
-                    <thead>
-                    <tr>
-                        <th><?= trans('Login'); ?></th>
-                        <th><?= trans('Nom'); ?></th>
-                        <th><?= trans('Prénom'); ?></th>
-                        <th><?= trans('Email'); ?></th>
-                        <th><?= trans('Rôle'); ?></th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php if (defined('ALLUSERS')):
-                        foreach (getAllUsers() as $userId => $user):
-                            if (getRoleId($user->role) <= getUserRoleId()): ?>
-                                <tr>
-                                    <td class="<?= $user->statut == 0 ? 'table-dark' : ''; ?>"><?= $user->login ?></td>
-                                    <td><?= $user->nom ?></td>
-                                    <td><?= $user->prenom ?></td>
-                                    <td><?= $user->email ?></td>
-                                    <td><?= getRoleName($user->role) ?></td>
-                                    <td>
-                                        <?php if ($user->statut > 0 && isEmail($user->email)): ?>
-                                            <button type="button" class="btn btn-sm defaultEmailUser"
-                                                    title="<?= trans('Définir comme adresse Email par défaut'); ?>"
-                                                <?= $defaultEmail == $user->email ? 'disabled="true"' : ''; ?>
-                                                    data-iduser="<?= $user->id ?>" data-email="<?= $user->email; ?>">
+        <div id="admin-tabs" class="col-12">
+            <?php foreach (getAllUsers() as $userId => $user):
+                if (getRoleId($user->role) <= getUserRoleId()): ?>
+                    <div class="admin-tab" data-user="<?= $user->id ?>"
+                         data-filter="<?= $user->login ?> <?= $user->nom ?> <?= $user->prenom ?> <?= $user->email ?> <?= getRoleName($user->role) ?>">
+                        <div class="admin-tab-header <?= $user->statut == 0 ? 'bg-secondary' : ''; ?>">
+                            <div class="col-12 col-sm-4 col-md-3 col-lg-3 col-xl-3"><?= $user->login ?></div>
+                            <div class="d-none d-sm-block col-sm-4 col-md-3 col-lg-2 col-xl-2"><?= $user->nom ?></div>
+                            <div class="d-none d-lg-block col-lg-2 col-xl-2"><?= $user->prenom ?></div>
+                            <div class="d-none d-md-block col-md-3 col-lg-3 col-xl-3"><?= $user->email ?></div>
+                            <div class="d-none d-sm-block col-sm-4 col-md-3 col-lg-2 col-xl-2"><?= getRoleName($user->role) ?></div>
+                        </div>
+                        <div class="admin-tab-content">
+                            <div class="admin-tab-content-header">
+                                <div>
+                                    <h2><?= $user->login ?></h2>
+                                    <?php if ($user->statut > 0 && isEmail($user->email)): ?>
+                                        <button type="button" class="btnLink defaultEmailUser"
+                                                title="<?= trans('Définir comme adresse Email par défaut'); ?>"
+                                            <?= $defaultEmail == $user->email ? 'disabled="true"' : ''; ?>
+                                                data-iduser="<?= $user->id ?>" data-email="<?= $user->email; ?>">
                                                 <span class="<?= $defaultEmail == $user->email ? 'text-success' : ''; ?>">
                                                     <i class="fas fa-envelope"></i></span>
-                                            </button>
-                                        <?php endif;
-                                        if (getUserIdSession() == $user->id || getUserRoleId() >= getRoleId($user->role)): ?>
-                                            <a href="<?= getUrl('user/', $user->id) ?>"
-                                               class="btn btn-sm" title="<?= trans('Modifier'); ?>">
-                                                <span class=""><i class="fas fa-wrench"></i></span>
-                                            </a>
-                                        <?php endif;
-                                        if ($user->id != getUserIdSession() && getUserRoleId() > getRoleId($user->role) && $user->statut > 0): ?>
-                                            <button type="button" class="btn btn-sm deleteUser"
-                                                    title="<?= trans('Bannir'); ?>"
-                                                    data-iduser="<?= $user->id ?>">
-                                                <span class="btnArchive"><i class="fas fa-ban"></i></span>
-                                            </button>
-                                        <?php endif;
-                                        if ($user->id != getUserIdSession() && getUserRoleId() > getRoleId($user->role) && $user->statut == 0 && isTechnicien(getUserRoleId())): ?>
-                                            <button type="button" class="btn btn-sm valideUser"
-                                                    title="<?= trans('Valider'); ?>"
-                                                    data-iduser="<?= $user->id ?>">
-                                                <span class=""><i class="fas fa-user-check"></i></span>
-                                            </button>
-                                        <?php endif; ?>
-                                    </td>
-                                </tr>
-                            <?php endif;
-                        endforeach;
-                    endif; ?>
-                    </tbody>
-                </table>
-            </div>
+                                        </button>
+                                        |
+                                    <?php endif;
+                                    if (getUserIdSession() == $user->id || getUserRoleId() >= getRoleId($user->role)): ?>
+                                        <a href="<?= getUrl('user/', $user->id) ?>"
+                                           class="btnLink"><?= trans('Modifier'); ?></a>
+                                        |
+                                    <?php endif;
+                                    if ($user->id != getUserIdSession() && getUserRoleId() > getRoleId($user->role) && $user->statut > 0): ?>
+                                        <button type="button" class="btnLink bannishUser" data-iduser="<?= $user->id ?>">
+                                            <?= trans('Bannir'); ?></button>
+                                        |
+                                    <?php endif;
+                                    if ($user->id != getUserIdSession() && getUserRoleId() > getRoleId($user->role) && $user->statut == 0 && isTechnicien(getUserRoleId())): ?>
+                                        <button type="button" class="btnLink valideUser"
+                                                data-iduser="<?= $user->id ?>"><?= trans('Valider'); ?></button>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <div class="px-2">
+                                <div class="my-3">
+                                    <strong><?= trans('ID'); ?></strong><?= $user->id ?>
+                                </div>
+                                <div class="my-3">
+                                    <strong><?= trans('Login'); ?></strong><?= $user->login ?>
+                                </div>
+                                <div class="my-3">
+                                    <strong><?= trans('Nom'); ?></strong><?= $user->nom ?>
+                                </div>
+                                <div class="my-3">
+                                    <strong><?= trans('Prénom'); ?></strong><?= $user->prenom ?>
+                                </div>
+                                <div class="my-3">
+                                    <strong><?= trans('Email'); ?></strong><?= $user->email ?>
+                                </div>
+                                <div class="my-3">
+                                    <strong><?= trans('Rôle'); ?></strong><?= getRoleName($user->role) ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endif;
+            endforeach; ?>
         </div>
     </div>
+<?php endif; ?>
     <script type="text/javascript" src="/app/lib/template/js/user.js"></script>
 <?php require('footer.php'); ?>
